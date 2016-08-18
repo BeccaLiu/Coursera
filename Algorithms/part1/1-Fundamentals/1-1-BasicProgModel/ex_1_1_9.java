fde43bdbea5c30067830fd51d510ad011fbe5853
BeccaLiu <rliu@enphaseenergy.com>
2016-08-17 22:00:33 -0700

    ex_1_1_9

---
+++ Algorithms/part1/1-Fundamentals/1-1-BasicProgModel/ex_1_1_9.java
@@ -0,0 +1,14 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class ex_1_1_9 {
+    public static void main(String[] args) {
+        StdOut.print("input N: \n");
+        int N = StdIn.readInt();
+        String output = "";
+        for (int n = N; n > 0; n /= 2) {
+            output = n % 2 + output;
+        }
+        StdOut.print(output);
+    }
+}
