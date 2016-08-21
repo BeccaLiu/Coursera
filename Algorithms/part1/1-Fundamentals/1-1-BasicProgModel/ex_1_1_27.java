ff7f28450c951d843353f66e0d63778302a2f956
BeccaLiu <rliu@enphaseenergy.com>
2016-08-20 19:54:07 -0700

    ex_1_1_27 error corrected

--- Algorithms/part1/1-Fundamentals/1-1-BasicProgModel/ex_1_1_27.java
+++ Algorithms/part1/1-Fundamentals/1-1-BasicProgModel/ex_1_1_27.java
@@ -4,20 +4,19 @@ import edu.princeton.cs.algs4.StdOut;
 public class ex_1_1_27 {
 
     public static void main(String[] args) {
-        StdOut.print("Input N");
+        StdOut.print("Input N: ");
         int N = StdIn.readInt();
-        StdOut.print("Input K");
+        StdOut.print("Input K: ");
         int K = StdIn.readInt();
-        StdOut.print("Input p");
-        int p = StdIn.readInt();
+        StdOut.print("Input p: ");
+        float p = StdIn.readFloat();
 
-        //StdOut.println(binomial(100, 50, 0.3));
+        StdOut.println(binomial(N, K, p));
         double[][] array = new double[N + 1][K + 1];
-        for (int i = 0; i <= K; i++) {
-            array[0][i] = 1;
-        }
+        array[0][0] = 1;
+
         for (int i = 1; i <= N; i++) {
-            array[i][0] = (1 - p) * array[i - 1][0] + p * 1;
+            array[i][0] = (1 - p) * array[i - 1][0];
         }
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= K; j++) {
@@ -28,9 +27,11 @@ public class ex_1_1_27 {
 
     }
 
-    private static double binomial(int N, int k, double p) { //recursive
-        if ((N == 0) || (k < 0))
+    private static double binomial(int N, int k, double p) { //recursive slow
+        if (N == 0 && k == 0)
             return 1.0;
+        if (N < 0 || k < 0)
+            return 0.0;
         return (1.0 - p) * binomial(N - 1, k, p) + p * binomial(N - 1, k - 1, p);
     }
 
